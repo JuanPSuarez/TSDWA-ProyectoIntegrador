5c0199cc6ca36a0b65a831cb3181a099670110c7
Juan Pablo Suarez <jp.juansuarez@gmail.com>
2023-09-18 23:43:21 -0300

    Se agregan archivos carpeta models. Se agrega archivos base Se agrega codigo en user.py

---
+++ BACKEND/models/user.py
@@ -0,0 +1,9 @@
+class User:
+    def __init__(self, userId, password, status, registerDate):
+        self.userId = userId
+        self.password = password
+        self.status = status
+        self.registerDate = registerDate
+
+    def verifyLogin(self):
+        pass
