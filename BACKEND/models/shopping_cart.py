a41b837aff4affe838da3dbc170415b5eb14f0cc
Juan Pablo Suarez <jp.juansuarez@gmail.com>
2023-09-18 23:49:51 -0300

    Se agrega codigo en: author.py book.py genre.py order.py relationship.py shopping_cart.py user.py Se agrega imports necesarios en main.py

---
+++ BACKEND/models/shopping_cart.py
@@ -0,0 +1,19 @@
+class Shopping_Cart:
+    def __init__(self, cartId, title, quantity, totalPurchase, dateAdded):
+        self.cartId = cartId
+        self.title = title
+        self.quantity = quantity
+        self.totalPurchase = totalPurchase
+        self.dateAdded = dateAdded
+
+    def addCartItem(self):
+        pass
+
+    def updateQuantity(self):
+        pass
+
+    def viewCartDetails(self):
+        pass
+
+    def checkOut(self):
+        pass
--- BACKEND/models/user.py
+++ BACKEND/models/user.py
@@ -7,3 +7,30 @@ class User:
 
     def verifyLogin(self):
         pass
+
+class Costumer(User):
+    def __init__(self, costumerId, email, dni, telephone, suscription):
+        super().__init__(costumerId, "", "", None)
+        self.email = email
+        self.dni = dni
+        self.telephone = telephone
+        self.suscription = suscription
+
+    def register(self):
+        pass
+
+    def login(self):
+        pass
+
+    def recoverpassword(self):
+        pass
+
+
+class Admin(User):
+    def __init__(self, adminName, email):
+        super().__init__("", "", "", None)
+        self.adminName = adminName
+        self.email = email
+
+    def updateCat(self):
+        pass
