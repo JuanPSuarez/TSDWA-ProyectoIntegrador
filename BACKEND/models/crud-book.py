1772283f4122217278e9c51f9186beb7467832ca
leandro-santiago <69202675+leandros1793@users.noreply.github.com>
2023-10-19 15:59:17 -0300

    se creo el update_book

--- BACKEND/models/crud-book.py
+++ BACKEND/models/crud-book.py
@@ -6,3 +6,16 @@ class BookCRUD:
         new_book = Book(title, author, disponibilidad, price, sinopsis, editorial, ISBN, num_paginas, idioma, formato, clasificacion, publicationDate)
         Book.books.append(new_book)
         return "Libro creado exitosamente."
+    
+    @staticmethod
+    def read_books():
+        return Book.books
+
+    @staticmethod
+    def update_book(book_id, title, author, disponibilidad, price, sinopsis, editorial, ISBN, num_paginas, idioma, formato, clasificacion, publicationDate):
+        if 0 <= book_id < len(Book.books):
+            updated_book = Book(title, author, disponibilidad, price, sinopsis, editorial, ISBN, num_paginas, idioma, formato, clasificacion, publicationDate)
+            Book.books[book_id] = updated_book
+            return "Libro actualizado exitosamente."
+        else:
+            return "ID de libro no válido."
